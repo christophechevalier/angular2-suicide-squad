f6c8d646fde293cf746f99917ac0cd58275ac709
Christophe Chevalier <cchevalier@linagora.com>
2016-10-03 17:00:19 +0200

    Update app with new version of Angular Material 2 alpha.9
    - Add custom theme css file
    - Simplify usage of angular 2 modules by @angular/material
    - Create route for Dashboard & Heroes
    - Update Cosmetics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,17 @@
-import {Component} from '@angular/core';
+import {Component, ViewContainerRef} from '@angular/core';
+import {MdDialog, MdDialogConfig} from "@angular/material";
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'settings-dialog',
+  template: `
+    <label>Just a simple test ...</label>
+    <md-slide-toggle></md-slide-toggle>`
+})
+
+export class SettingsDialog {
+
+}
 
 @Component({
   selector: 'app-root',
@@ -8,5 +21,31 @@ import {Component} from '@angular/core';
 
 export class AppComponent {
   title = 'Tour of Heroes / Suicide Squad';
-}
+  
+  isDarkTheme = false;
 
+  tabs: Array <{title:string, url:string}>;
+
+  constructor(private dialog: MdDialog, private vcr: ViewContainerRef, private router: Router) {
+    this.tabs = [
+      {
+        title: 'Dashboard',
+        url: 'dashboard'
+      },
+      {
+        title: 'Heroes',
+        url: 'heroes'
+      }
+    ]
+  }
+
+  openDialog() {
+    const config = new MdDialogConfig();
+    config.viewContainerRef = this.vcr;
+    this.dialog.open(SettingsDialog, config);
+  }
+  
+  openTab(index) {
+    this.router.navigate(['/', this.tabs[index].url]);
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,22 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
-import { HttpModule }    from '@angular/http';
+import {NgModule}      from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule}   from '@angular/forms';
+import {HttpModule}    from '@angular/http';
 
 // Imports for loading & configuring the in-memory web api
-import { InMemoryWebApiModule } from 'angular2-in-memory-web-api';
-import { InMemoryDataService }  from './in-memory-data.service';
+import {InMemoryWebApiModule} from 'angular2-in-memory-web-api';
+import {InMemoryDataService}  from './in-memory-data.service';
 
-import { AppComponent }         from './app.component';
-import { DashboardComponent }   from './dashboard/dashboard.component';
-import { HeroesComponent }      from './heroes/heroes.component';
-import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
-import { HeroService }          from './hero.service';
-import { HeroSearchComponent } from './hero-search/hero-search.component';
-import { Routing }              from './app.routing';
+import {AppComponent, SettingsDialog}         from './app.component';
+import {DashboardComponent}   from './dashboard/dashboard.component';
+import {HeroesComponent}      from './heroes/heroes.component';
+import {HeroDetailComponent}  from './hero-detail/hero-detail.component';
+import {HeroService}          from './hero.service';
+import {HeroSearchComponent} from './hero-search/hero-search.component';
+import {Routing}              from './app.routing';
 
-import { MdButtonModule } from '@angular2-material/button';
-import { MdButtonToggleModule } from '@angular2-material/button-toggle';
-import { MdGridListModule } from '@angular2-material/grid-list/grid-list';
-import { MdListModule } from '@angular2-material/list/list';
-import { MdMenuModule } from '@angular2-material/menu';
-import { MdRadioModule } from '@angular2-material/radio';
-import { MdSliderModule } from '@angular2-material/slider';
-import { MdCardModule } from '@angular2-material/card';
-import { MdToolbarModule } from '@angular2-material/toolbar';
-import { MdInputModule } from '@angular2-material/input';
-/*import { MdTooltipModule } from '@angular2-material/tooltip';*/
+// Import All Material Modules
+import {MaterialModule} from '@angular/material';
 
 @NgModule({
   imports: [
@@ -35,21 +26,20 @@ import { MdInputModule } from '@angular2-material/input';
     InMemoryWebApiModule.forRoot(InMemoryDataService),
     Routing,
     [
-      MdButtonModule.forRoot(),
-      MdButtonToggleModule.forRoot(),
-      MdGridListModule,
-      MdListModule,
-      MdMenuModule,
-      MdRadioModule,
-      MdSliderModule,
-      MdCardModule,
-      MdToolbarModule,
-      MdInputModule,
-/*      MdTooltipModule.forRoot()*/
+      MaterialModule.forRoot()
     ],
   ],
   declarations: [
     AppComponent,
+    SettingsDialog,
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+    HeroSearchComponent,
+  ],
+  entryComponents: [
+    AppComponent,
+    SettingsDialog,
     DashboardComponent,
     HeroDetailComponent,
     HeroesComponent,
@@ -58,7 +48,7 @@ import { MdInputModule } from '@angular2-material/input';
   providers: [
     HeroService,
   ],
-  bootstrap: [ AppComponent ]
+  bootstrap: [AppComponent]
 })
 export class AppModule {
 }
